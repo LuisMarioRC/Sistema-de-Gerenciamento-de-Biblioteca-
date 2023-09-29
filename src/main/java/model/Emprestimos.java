a0d15c136325fe94dd191764b7ec80f0c33d2dd4
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-09-29 14:18:15 -0300

    :construction: feat - Add verificações no construtor

--- src/main/java/model/Emprestimos.java
+++ src/main/java/model/Emprestimos.java
@@ -3,11 +3,13 @@ package model;
 import dao.DAO;
 import dao.excecoes.EmprestimosException;
 import dao.excecoes.LivroException;
+import dao.excecoes.ReservaException;
 import dao.excecoes.UsuarioException;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Emprestimos {
@@ -19,23 +21,33 @@ public class Emprestimos {
     private Boolean andamento; //true = o emprestimo esta ativo, false = o emprestimo foi delvolvido
 
 
-    public Emprestimos(Livro livro, Usuario usuario,String dataHoje) throws EmprestimosException, LivroException {
+    public Emprestimos(Livro livro, Usuario usuario,String dataHoje) throws EmprestimosException, LivroException, ReservaException {
         //verificando se o usuario nao tem livro atrasado, se o livro está disponivel, se n tem reserva para esse livro,
-        // se o usuario é o primeiro na lista de reserva, se o usuario não esta bloqueado e se tem multa.
+        // se o usuario não esta bloqueado,se tem multa e o total de emprestimos.
+        ArrayList<Emprestimos> emprestimosDoUsuario = DAO.getEmprestimosDAO().econtraPorUsuario(usuario);
+        int totalDeEmprestimos = emprestimosDoUsuario.size();
         if (!DAO.getEmprestimosDAO().verificaAtrasoDeUsuario(usuario)
+                //verificando informações referente ao usuario
                 && livro.getDisponibilidade()
-                && DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario)
                 && usuario.getStatus()
                 && usuario.getMulta() == 0
-                && usuario.getNumEmprestimos() <= 3) {
-            DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            livro.setDisponibilidade(false);
-            DAO.getLivroDAO().atualizar(livro);
-            this.livro = livro;
-            this.usuario = usuario;
-            this.dataEmprestimos = LocalDate.parse(dataHoje,dataFormatada);
-            this.dataDevolucao = dataEmprestimos.plus(Period.ofDays(7));
-            this.setAndamento(true); //true significa ativo;
+                && totalDeEmprestimos <3) {
+            //verificando se a lista ta de reserva desse livro ta vazia ou o usuario é o primeiro da lista para emprestar
+            if (DAO.getReservaDAO().getReservasParaLivro(livro.getId()).size() ==0
+                    ||  DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario)) {
+                //Se for o primeiro da lista, chama a função para retirar da lista;
+                if (DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario) ) {
+                    DAO.getReservaDAO().retiraUsuarioDaLista(livro.getId(),usuario);
+                }
+                DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                livro.setDisponibilidade(false);
+                DAO.getLivroDAO().atualizar(livro);
+                this.livro = livro;
+                this.usuario = usuario;
+                this.dataEmprestimos = LocalDate.parse(dataHoje, dataFormatada);
+                this.dataDevolucao = dataEmprestimos.plus(Period.ofDays(7));
+                this.andamento = true; //true significa ativo;
+            }
         }else{
             throw new EmprestimosException(EmprestimosException.EMPRESTAR);
         }
