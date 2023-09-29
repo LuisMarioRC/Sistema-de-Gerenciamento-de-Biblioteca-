abb0e0c667e7da1c5a034c76059f27231fc278fa
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-09-29 14:24:10 -0300

    :heavy_plus_sign: build - add de dependências do JUnit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.projetopbl {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires junit;
+
+
     opens com.example.projetopbl to javafx.fxml;
     exports com.example.projetopbl;
 }
