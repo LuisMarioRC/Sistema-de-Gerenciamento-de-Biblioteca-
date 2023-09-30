b3f34474b7903cba1f24c9996df93cafc2225f63
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-09-30 20:30:57 -0300

    :sparkles: feat - Reorganização de verificação no construtor e criação do método para renovar.

--- src/main/java/model/Emprestimos.java
+++ src/main/java/model/Emprestimos.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class Emprestimos {
@@ -21,48 +20,49 @@ public class Emprestimos {
     private Boolean andamento; //true = o emprestimo esta ativo, false = o emprestimo foi delvolvido
 
 
-    public Emprestimos(Livro livro, Usuario usuario,String dataHoje) throws EmprestimosException, LivroException, ReservaException, UsuarioException {
-        //verificando se o usuario nao tem livro atrasado, se o livro está disponivel, se n tem reserva para esse livro,
-        // se o usuario não esta bloqueado,se tem multa e o total de emprestimos.
-        ArrayList<Emprestimos> emprestimosDoUsuario = DAO.getEmprestimosDAO().econtraPorUsuario(usuario);
-        int totalDeEmprestimos = emprestimosDoUsuario.size();
-        if (!DAO.getEmprestimosDAO().verificaAtrasoDeUsuario(usuario)
-                //verificando informações referente ao usuario
-                && livro.getDisponibilidade()
-                && usuario.getStatus()
-                &&  DAO.getEmprestimosDAO().validaMulta(usuario,dataHoje)
-                && totalDeEmprestimos <3) {
-            //verificando se a lista ta de reserva desse livro ta vazia ou o usuario é o primeiro da lista para emprestar
-            if (DAO.getReservaDAO().getReservasParaLivro(livro.getId()).isEmpty()
-                    ||  DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario)) {
-                //Se for o primeiro da lista, chama a função para retirar da lista;
-                if (DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario) ) {
-                    DAO.getReservaDAO().retiraUsuarioDaLista(livro.getId(),usuario);
-                }
-                DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                livro.setDisponibilidade(false);
-                DAO.getLivroDAO().atualizar(livro);
-                usuario.setFimDaMulta(null);
-                DAO.getUsuarioDAO().atualizar(usuario);
-                this.livro = livro;
-                this.usuario = usuario;
-                this.dataEmprestimos = LocalDate.parse(dataHoje, dataFormatada);
-                this.dataDevolucao = dataEmprestimos.plus(Period.ofDays(7));
-                this.andamento = true; //true significa ativo;
-            }
-        }else{
-            throw new EmprestimosException(EmprestimosException.EMPRESTAR);
+    public Emprestimos(Livro livro, Usuario usuario,String dataHoje) throws LivroException, ReservaException, UsuarioException {
+        if (DAO.getEmprestimosDAO().verificaAtrasoDeUsuario(usuario)) {
+            throw new UsuarioException(UsuarioException.ATRASO);
         }
+        if (!livro.getDisponibilidade()){
+            throw new LivroException(LivroException.INDISPONIBILIDADE);
+        }
+        if (!usuario.getStatus()){
+            throw new UsuarioException(UsuarioException.BLOQUEIO);
+        }
+        if (!DAO.getEmprestimosDAO().validaMulta(usuario,dataHoje)){
+            throw new UsuarioException(UsuarioException.MULTADO);
+        }
+        if (DAO.getEmprestimosDAO().econtraPorUsuario(usuario).size() >= 3){
+            throw new UsuarioException(UsuarioException.LIMITE);
+        }
+        if (!DAO.getReservaDAO().getReservasParaLivro(livro.getId()).isEmpty()) {
+            if (DAO.getReservaDAO().primeiroUsuarioNaLista(livro.getId(),usuario) ) {
+                DAO.getReservaDAO().retiraUsuarioDaLista(livro.getId(),usuario);
+            }else{
+                throw new ReservaException(ReservaException.RESERVADO);
+            }
+        }
+        DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        livro.setDisponibilidade(false);
+        DAO.getLivroDAO().atualizar(livro);
+        usuario.setFimDaMulta(null);
+        DAO.getUsuarioDAO().atualizar(usuario);
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimos = LocalDate.parse(dataHoje, dataFormatada);
+        this.dataDevolucao = dataEmprestimos.plus(Period.ofDays(7));
+        this.andamento = true; //true significa ativo;
     }
+
 
 
     private void multas (Livro livro, Usuario usuario) throws EmprestimosException, UsuarioException {
         LocalDate datahoje = LocalDate.now();
         Emprestimos emprestimo = DAO.getEmprestimosDAO().encontraPorIdDoLivro(livro.getId());
         if (emprestimo != null) {
-            // verificar se tem emprestimos com esse livro;
-            long diferencaEntreDias = ChronoUnit.DAYS.between(datahoje, emprestimo.getDataDevolucao());
-            if (diferencaEntreDias > 0) { // para calcular a multa, tem que ter diferença de dias
+            if (datahoje.isAfter(emprestimo.getDataDevolucao())) {
+                long diferencaEntreDias = ChronoUnit.DAYS.between(datahoje, emprestimo.getDataDevolucao());
                 int diasDeMulta = Math.toIntExact(diferencaEntreDias * 2);
                 usuario.setFimDaMulta(emprestimo.getDataDevolucao().plusDays(diasDeMulta));
                 DAO.getUsuarioDAO().atualizar(usuario);
@@ -70,14 +70,37 @@ public class Emprestimos {
         }
     }
 
-    public Livro registraDevolucao(Livro livro, Usuario usuario) throws EmprestimosException, LivroException, UsuarioException {
+    public void registraDevolucao(Livro livro, Usuario usuario) throws EmprestimosException, LivroException, UsuarioException {
         this.multas(livro,usuario);
         Emprestimos emprestimoDoLivro = DAO.getEmprestimosDAO().encontraPorIdDoLivro(livro.getId());
         emprestimoDoLivro.setAndamento(false);
         DAO.getEmprestimosDAO().atualizar(emprestimoDoLivro);
         livro.setDisponibilidade(true);
         DAO.getLivroDAO().atualizar(livro);
-        return livro;
+    }
+
+    public void renovar(Livro livro, Usuario usuario, String dataHoje) throws EmprestimosException, UsuarioException, ReservaException {
+        Emprestimos emprestimo = DAO.getEmprestimosDAO().encontraPorIdDoLivro(livro.getId());
+        DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate newDate = LocalDate.parse(dataHoje, dataFormatada);
+        if (DAO.getEmprestimosDAO().verificaAtrasoDeUsuario(usuario)) {
+            throw new UsuarioException(UsuarioException.ATRASO);
+        }
+        if (DAO.getReservaDAO().verificaReserva(livro.getId())){
+            throw new ReservaException(ReservaException.RESERVADO);
+        }
+        if (!DAO.getEmprestimosDAO().validaMulta(usuario,dataHoje)){
+            throw new UsuarioException(UsuarioException.MULTADO);
+        }
+        if (!usuario.getStatus()){
+            throw new UsuarioException(UsuarioException.BLOQUEIO);
+        }
+        if (ChronoUnit.DAYS.between(emprestimo.getDataEmprestimos(),newDate) >=14 ){
+            throw new EmprestimosException(EmprestimosException.RENOVAR);
+        }
+        LocalDate dataDeDevolucao = emprestimo.getDataDevolucao();
+        emprestimo.setDataDevolucao(dataDeDevolucao.plusDays(7));
+        DAO.getEmprestimosDAO().atualizar(emprestimo);
     }
 
 
@@ -150,7 +173,7 @@ public class Emprestimos {
                 ", dataEmprestimos=" + dataEmprestimos +
                 ", dataDevolucao=" + dataDevolucao +
                 ", id=" + id +
+                ", andamento=" + andamento +
                 '}';
     }
-
 }
