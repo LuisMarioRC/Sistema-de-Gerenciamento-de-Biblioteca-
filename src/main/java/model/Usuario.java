037c4444d54cfa8fca8935d61aaaf49e1bf7785b
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-10-01 20:45:58 -0300

    :sparkles: feat - Adcionando atributos na própria classe.

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -4,20 +4,25 @@ import dao.DAO;
 import dao.excecoes.UsuarioException;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Usuario extends Operador{
+public class Usuario {
 
     private String endereco;
     private String telefone;
     private LocalDate fimDaMulta;
+    private String nome;
+    private Boolean status; //true = ativo, false= bloqueado
+    private Integer numeroDeIdentificacao;
 
 
 
     public Usuario(String nome,String endereco,String telefone){
-        super(nome);
+        this.nome=nome;
         this.endereco=endereco;
         this.telefone=telefone;
         this.fimDaMulta = null;
+        this.status=true;
     }
 
     public Usuario bloquearConta(Usuario usuario) throws UsuarioException{
@@ -42,17 +47,6 @@ public class Usuario extends Operador{
         this.telefone = telefone;
     }
 
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "nome=" + super.getNome() + '\''+
-                ",endereco=" + endereco + '\'' +
-                ", telefone=" + telefone + '\'' +
-                ", numeroDeIdentificacao=" + super.getNumeroDeIdentificacao()+ '\''+
-                ",status=" +super.getStatus()+ '\''+
-                ", fimDaMulta= "+ fimDaMulta+
-                '}';
-    }
 
     public LocalDate getFimDaMulta() {
         return fimDaMulta;
@@ -60,5 +54,54 @@ public class Usuario extends Operador{
 
     public void setFimDaMulta(LocalDate fimDaMulta) {
         this.fimDaMulta = fimDaMulta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Integer getNumeroDeIdentificacao() {
+        return numeroDeIdentificacao;
+    }
+
+    public void setNumeroDeIdentificacao(Integer numeroDeIdentificacao) {
+        this.numeroDeIdentificacao = numeroDeIdentificacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(numeroDeIdentificacao, usuario.numeroDeIdentificacao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numeroDeIdentificacao);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", fimDaMulta=" + fimDaMulta +
+                ", nome='" + nome + '\'' +
+                ", status=" + status +
+                ", numeroDeIdentificacao=" + numeroDeIdentificacao +
+                '}';
     }
 }
