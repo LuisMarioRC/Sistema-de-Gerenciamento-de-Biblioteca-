a35f4b746fcf5105c55809479679e5472a64b306
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-10-01 20:38:38 -0300

    :white_check_mark: test - Realocando métodos de teste para outra classe.

--- src/main/java/test/ReservaDAOTest.java
+++ src/main/java/test/ReservaDAOTest.java
@@ -1,18 +1,15 @@
 package test;
 
 import dao.DAO;
-import dao.excecoes.EmprestimosException;
 import dao.excecoes.LivroException;
 import dao.excecoes.ReservaException;
 import dao.excecoes.UsuarioException;
-import model.Emprestimos;
 import model.Livro;
 import model.Reserva;
 import model.Usuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -47,51 +44,6 @@ public class ReservaDAOTest {
         DAO.getReservaDAO().excluirTodos();
     }
 
-    @Test
-    void failreservaConstrutorStatusBloqueado() throws UsuarioException,LivroException {
-        usuario1.setStatus(false);
-        DAO.getUsuarioDAO().atualizar(usuario1);
-        try{
-            new Reserva(0,usuario1,"01/10/2023");
-            fail("Uma exeção deveria ser lançada");
-        } catch (UsuarioException e) {
-            assertEquals(UsuarioException.BLOQUEIO,e.getMessage());
-        }
-    }
-    @Test
-    void failreservaConstrutorUsuarioMulta() throws UsuarioException,LivroException {
-        usuario1.setFimDaMulta(LocalDate.of(2023,10,2));
-        DAO.getUsuarioDAO().atualizar(usuario1);
-        try{
-            new Reserva(0,usuario1,"01/10/2023");
-            fail("Uma exeção deveria ser lançada");
-        } catch (UsuarioException e) {
-            assertEquals(UsuarioException.MULTADO,e.getMessage());
-        }
-    }
-    @Test
-    void failreservaConstrutorUsuarioAtrasado() throws UsuarioException, LivroException, ReservaException, EmprestimosException {
-        Emprestimos atrasado =DAO.getEmprestimosDAO().criar(new Emprestimos(livro2,usuario1,"01/09/2023"));
-        atrasado.setDataDevolucao(LocalDate.of(2023,9,30));
-        DAO.getEmprestimosDAO().atualizar(atrasado);
-        try{
-            new Reserva(0,usuario1,"01/10/2023");
-            fail("Uma exeção deveria ser lançada");
-        } catch (UsuarioException e) {
-            assertEquals(UsuarioException.ATRASO,e.getMessage());
-        }
-    }
-    @Test
-    void failreservaConstrutorLivroDisponivel() throws UsuarioException, LivroException {
-        livro2.setDisponibilidade(true);
-        DAO.getLivroDAO().atualizar(livro2);
-        try{
-            new Reserva(livro2.getId(),usuario1,"01/10/2023");
-            fail("Uma exeção deveria ser lançada");
-        } catch (LivroException e) {
-            assertEquals(LivroException.DISPONIBILIDADE,e.getMessage());
-        }
-    }
 
     @Test
     void criar() throws LivroException, UsuarioException {
