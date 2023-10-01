ae41500e0897f5c527d34bf1d89941932bdcbc44
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-10-01 20:37:10 -0300

    sparkles: feat- Adcionando atributos na própria classe.

--- src/main/java/model/Bibliotecario.java
+++ src/main/java/model/Bibliotecario.java
@@ -1,15 +1,77 @@
 package model;
 
+import java.util.Objects;
 
-public class Bibliotecario extends Operador {
+public class Bibliotecario  {
+    private String nome;
+    private Integer senha;
+    private String cargo;
+    private Integer numeroDeIdentificacao;
 
     public Bibliotecario(String nome,Integer senha){
-        super(nome,senha);
+        this.nome=nome;
+        this.senha=senha;
+        this.cargo="Biliotecario";
+    }
 
+    public Bibliotecario(String nome,Integer senha,String cargo){
+        this.nome=nome;
+        this.senha=senha;
+        this.cargo=cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getSenha() {
+        return senha;
+    }
+
+    public void setSenha(Integer senha) {
+        this.senha = senha;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Integer getNumeroDeIdentificacao() {
+        return numeroDeIdentificacao;
+    }
+
+    public void setNumeroDeIdentificacao(Integer numeroDeIdentificacao) {
+        this.numeroDeIdentificacao = numeroDeIdentificacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bibliotecario that = (Bibliotecario) o;
+        return Objects.equals(numeroDeIdentificacao, that.numeroDeIdentificacao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numeroDeIdentificacao);
     }
 
     @Override
     public String toString() {
-        return "Bibliotecario{}";
+        return "Bibliotecario{" +
+                "nome='" + nome + '\'' +
+                ", senha=" + senha +
+                ", cargo='" + cargo + '\'' +
+                ", numeroDeIdentificacao=" + numeroDeIdentificacao +
+                '}';
     }
 }
