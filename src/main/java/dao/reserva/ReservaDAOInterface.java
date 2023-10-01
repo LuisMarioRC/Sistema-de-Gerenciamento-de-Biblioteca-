d560a0c292e8fb578f789c93c11112313810bde1
GHenryssg <gabrielhenrysilva10@gmail.com>
2023-10-01 17:52:45 -0300

    :boom: fix - Correção de métodos na interface

--- src/main/java/dao/reserva/ReservaDAOInterface.java
+++ src/main/java/dao/reserva/ReservaDAOInterface.java
@@ -5,16 +5,15 @@ import model.Reserva;
 import model.Usuario;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface ReservaDAOInterface extends CRUD<Reserva, Exception> {
     public boolean primeiroUsuarioNaLista(Integer idLivro, Usuario usuario);
     public boolean verificaReserva(Integer idLivro);
-    public void retiraUsuarioDaLista(Integer idLivro , Usuario usuario);
+    public void retiraUsuarioDaLista(Integer idLivro);
     public ArrayList<Reserva> getReservasParaLivro(Integer idLivro);
     public Integer numLivrosReservados();
 
 
-
-
+    public Map<Integer, ArrayList<Reserva>> getReservas();
 }
-
