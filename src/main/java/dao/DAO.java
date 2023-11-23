c636950e21de57924fed502911c2a943ce846f5e
LuisMarioRC <luismariocarneiro8@gmail.com>
2023-11-23 19:57:19 -0300

    :books: docs - Adição de comentários no padrão javaDoc

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -9,6 +9,7 @@ import dao.usuario.UsuarioDAO;
 
 /**
  * Classe responável por fornecer instâncias de todas as classes DAO's
+ * Atributos estáticos intanciando cada classe
  * @author Luis Mario
  * @author Gabriel Henry
  * @see dao.administrador.AdministradorDAO
@@ -19,7 +20,6 @@ import dao.usuario.UsuarioDAO;
  * @see dao.usuario.UsuarioDAO
  */
 public class DAO {
-
     private static LivroDAO livroDao;
     private static EmprestimosDAO emprestimosDao;
     private static UsuarioDAO usuarioDao;
@@ -28,41 +28,54 @@ public class DAO {
     private static ReservaDAO reservaDao;
 
 
+    /**
+     * @return A instância de code ReservaDAO.
+     */
     public static ReservaDAO getReservaDAO(){
         if (reservaDao == null) {
             reservaDao = new ReservaDAO();
         }
         return reservaDao;
     }
-
+    /**
+     * @return A instância de code LivroDAO.
+     */
     public static LivroDAO getLivroDAO() {
         if (livroDao == null) {
             livroDao = new LivroDAO();
         }
         return livroDao;
     }
-
+    /**
+     * @return A instância de code EmprestimosDAO.
+     */
     public static EmprestimosDAO getEmprestimosDAO() {
         if (emprestimosDao == null) {
             emprestimosDao = new EmprestimosDAO();
         }
         return emprestimosDao;
     }
-
+    /**
+     * @return A instância de code UsuarioDAO.
+     */
     public static UsuarioDAO getUsuarioDAO() {
         if (usuarioDao == null) {
             usuarioDao = new UsuarioDAO();
         }
         return usuarioDao;
     }
-
+    /**
+     * @return A instância de code AdministradorDAO.
+     */
     public static AdministradorDAO getAdministradorDAO() {
         if (administradorDao == null) {
             administradorDao = new AdministradorDAO();
         }
         return administradorDao;
     }
-
+    /**
+     * @return A instância de code BibliotecarioDAO.
+     */
     public static BibliotecarioDAO getBibliotecarioDAO() {
         if (bibliotecarioDao == null) {
             bibliotecarioDao = new BibliotecarioDAO();
